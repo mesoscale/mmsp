be69e07afe1f086ac3bb1d948be859531ae60efa
gruberja <gruberja@66ccb129-c526-0410-9000-57a8edc78068>
2013-02-24 19:27:44 +0000

    changing to new major version
    git-svn-id: https://matforge.org/svn/mmsp/MMSP@41 66ccb129-c526-0410-9000-57a8edc78068

---
+++ algorithms/topology/point.hpp
@@ -0,0 +1,86 @@
+#ifndef _Point_h_
+#define _Point_h_
+#include <iostream>
+#include <cmath>
+
+template <class T> class Point
+{
+public:
+  // Representation
+  T x, y, z;
+  // Constructors
+  Point<T>(T a = 0, T b = 0, T c = 0): x(a), y(b), z(c) {}
+  Point<T>(const Point<T>& old): x(old.x), y(old.y), z(old.z) {}
+  template <class U>
+  Point<T>(const Point<U>& old): x(old.x), y(old.y), z(old.z) {}
+  // Modifiers
+  Point& operator+=(const Point<T>& rhs)
+  {
+    this->x += rhs.x;
+    this->y += rhs.y;
+    this->z += rhs.z;
+    return (*this);
+  }
+  Point& operator-=(const Point<T>& rhs)
+  {
+    this->x -= rhs.x;
+    this->y -= rhs.y;
+    this->z -= rhs.z;
+    return (*this);
+  }
+  template <class U>
+  Point<T>& operator*=(U rhs)
+  {
+    T n(rhs);
+    this->x=x*n;
+    this->y=y*n;
+    this->z=z*n;
+    return (*this);
+  }
+  template <class U>
+  Point<T>& operator/=(U rhs)
+  {
+    T n(rhs);
+    this->x=x/n;
+    this->y=y/n;
+    this->z=z/n;
+    return (*this);
+  }
+};
+
+// Arithmetic operators: Not member functions, but helpful utilities.
+template <class T>
+Point<T> operator+(const Point<T>& p, const Point<T>& q){return Point<T>(p.x+q.x, p.y+q.y, p.z+q.z);}
+
+template <class T>
+Point<T> operator-(const Point<T>& p, const Point<T>& q){return Point<T>(p.x-q.x, p.y-q.y, p.z-q.z);}
+
+template <class T>
+Point<T> operator*(const Point<T>& p, const Point<T>& q){return Point<T>(p.x*q.x, p.y*q.y, p.z*q.z);}
+
+template <class U, class T>
+Point<U> operator*(const U& f, const Point<T>& p){return Point<U>(f*p.x, f*p.y, f*p.z);}
+
+template <class T>
+Point<T> operator/(const Point<T>& p, const Point<T>& q){return Point<T>(p.x/q.x, p.y/q.y, p.z/q.z);}
+
+template <class T, class U>
+Point<T> operator/(const Point<T>& p, const U& f){return Point<T>(p.x/f, p.y/f, p.z/f);}
+
+
+// Point output operator
+//std::ostream& operator<<(std::ostream& out, Point<T>& p)
+template <class T> std::ostream& operator<<(std::ostream& out, const Point<T>& p)
+{
+  out<<'('<<p.x<<','<<p.y<<','<<p.z<<')';
+  return out;
+}
+
+// Distance between points
+template <class T>
+double distance_function( Point<T> p, Point<T> q)
+{
+  return sqrt((p.x-q.x)*(p.x-q.x) + (p.y-q.y)*(p.y-q.y) + (p.z-q.z)*(p.z-q.z));
+}
+
+#endif
---
+++ examples/solidification/anisotropic/phase_field/dendritic.cpp
@@ -0,0 +1,13 @@
+// snowflake.cpp
+// Isotropic snowflake code
+// Questions/comments to kellet@rpi.edu (Trevor Keller)
+
+#include"snowflake.hpp"
+
+std::string PROGRAM = "snowflake";
+std::string MESSAGE = "Anisotropic snowflake code";
+
+#include"MMSP.main.hpp"
+
+
+
