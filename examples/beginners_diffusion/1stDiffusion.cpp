a3e6d8321886fe56aaf6135cd72680b69624c039
daniellewis <lewisd2@rpi.edu>
2014-06-23 09:29:02 -0400

    Updates to 1st diffusion and manual.  Just the start.  A long way to go yet.

--- examples/beginners_diffusion/1stDiffusion.cpp
+++ examples/beginners_diffusion/1stDiffusion.cpp
@@ -1,9 +1,24 @@
-#include"MMSP.hpp"
-#include<math.h>
+// Uncomment the following definition if you have gnuplot installed on 
+// your system and you've want the gnuplot_i library linked into
+// this code.  This will permit interactive display of your results.
+// If you don't have gnuplot or gnuplot_i just leave that line commented
+// out.
+//
+// Add a line here about what the makefile or compilation command would 
+// look like.  Given the complexity of MMSP it might be OK to assume that 
+// folks can, in general, compile code.
+//
+#def GNUPLOT_I
+
+#ifdef GNUPLOT_I
+#include "gnuplot_i.h"
+#endif
+
+#include "MMSP.hpp"
+#include <math.h>
 using namespace MMSP;
 
-
-void main()
+int main()
 {
   // Here we set the critical parameters of the simulation.
   int number_points = 100;
@@ -13,34 +28,38 @@ void main()
   float simulation_time = 1.0;
   float diffusion_coefficient = 1.0;
 
+#ifdef GNUPLOT_I
+  gnuplot_ctrl *conc_plot;
+  double data[number_points];
+  conc_plot = gnuplot_init();
+#endif
+
   // Things that get computed at runtime are done here.
   // The timestep is determined by considering the Courant condition.
   //
-  float dt;
-  dt = (dx*dx)/(2*diffusion_coefficient);
+  float dt = (dx*dx)/(2*diffusion_coefficient);
   //
   // The length of the domain is computed from the grid spacing and the
   // number of points used.
   //
-  float length;
-  length = dx*number_points;
+  float length = dx*number_points;
   
   int timesteps;
   timesteps = simulation_time/dx;
-  // Does this automagially truncate the fractional part?
 
-  // Create the data structures to hold the concentration values.  Two grids
-  // are needed for storing the "new" and the "old" values of concentration.
+  // Create the data structures to hold the concentration values.  Two 
+  // grids are needed for storing the "new" and the "old" values of
+  // concentration.
   //
   // Short explanation of the data structure here.
   //
   MMSP::grid<1,scalar<float>> concentration_new(1,x_lower_bound,x_upper_bound);
   MMSP::grid<1,scalar<float>> concentration_old(1,x_lower_bound,x_upper_bound);
   
-  // We use two functions that are part of the grid class for accesing information
-  // about the grid.  x0() gives us the global lower bound of the grid passed as
-  // an argument.  x1() gives us the global upper bound of the grid passed as an
-  // argument.
+  // We use two functions that are part of the grid class for accessing
+  // information about the grid.  x0() gives us the global lower bound 
+  // of the grid passed as an argument.  x1() gives us the global upper
+  // bound of the grid passed as an argument.
   //
   // In this part of the code we are setting the initial conditions.
   //
@@ -70,8 +89,8 @@ void main()
     //
     coefficient = diffusion_coefficient*dt
     for (int i = x0(concentration_old)+1; i < x1(concentration_old)-1; i++) {
-      // We compute the laplacian on the grid points here.  Later this will be
-      // replaced by a function in MMSP.
+      // We compute the laplacian on the grid points here.  Later this 
+      // will be replaced by a function in MMSP.
       laplacian = (concentration_old[x-1]-2*concentration_old[x]+concentration_old[x+1])
       concentration_new[x]=coefficient*laplacian + concentration_old[x];
       // This closes the spatial iterating loop.
@@ -82,15 +101,41 @@ void main()
     concentration_new[x0(concentration_new)-1] = concentration_new[x0(concentration_new)-2]
     // Swap old and new concentrations for the next timestep.
     swap(concentration_old, concentration_new);
-    // This closes the timestep loop.
+
+    // This is where we print the output.
+#ifdef GNUPLOT_I
+    // IF you have gnuplot_i on your system, then do these lines.
+    
+    for (int i = x0(concentration_old); i < x1(concentration_old)-1; i++) {
+      // Store the data in an array for gnuplot_i.
+      data[i]=concentration)old[i];
+    }
+    gnuplot_resetplot(conc_plot);
+    gnuplot_cmd(conc_plot, "set nolabel");
+    gnuplot_cmd(conc_plot, "set yrange[-0.1:1.1]");
+    gnuplot_plot_x(conc_plot, data, number_points, "Concentration");
+    //
+#else
+    // ELSE print the data to the terminal.
+    for (int i = x0(concentration_old); i < x1(concentration_old)-1; i++) {
+      // print the data.
+      cout << concentration_old[i] << '\n';
+    }
+    //
+#endif
+
+    // This closes the timestep loop.  Go back to the top and do it again.
   }
-  // This prints the results of the grid to cout.  We print the old value here
-  // because of the swap up above.
-  for (int i = x0(concentration_old)+1; i < x1(concentration_old)-1; i++) {
-    std::cout<<concentration_old[x]<< "\n";
-    // Finishes the output loop.
-  }
+
+  // This cleans up MMSP, gnuplot and the code.
+
+#ifdef GNUPLOT_I
+  gnuplot_close(conc_plot);
+#endif
+
   Finalize();
+  // return success.
+  return 0;
   // This bracket ends main()
 }
 
