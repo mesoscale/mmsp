e2df3fdaa7c28f46a0ac4e14bc9ab306e6033ae1
fakemusleman <tanyx1990@gmail.com>
2015-06-18 11:18:37 -0400

    delete redundants

---
+++ examples/coarsening/grain_growth/anisotropic/Monte_Carlo/graingrowth.hpp
@@ -0,0 +1,12 @@
+// graingrowth.cpp
+// Questions/comments to tany3@rpi.edu (Yixuan Tan)
+#include<cstdio>
+#include"MMSP.hpp"
+
+std::string PROGRAM = "graingrowth";
+std::string MESSAGE = "Monte Carlo grain growth code";
+
+typedef MMSP::grid<1, int> GRID1D;
+typedef MMSP::grid<2, int> GRID2D;
+typedef MMSP::grid<3, int>  GRID3D;
+
