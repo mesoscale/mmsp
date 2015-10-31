a8593de6ccf49ceebcbf72dc421e3291ae4d84f0
Trevor Keller <trevor.keller@nist.gov>
2015-10-31 15:08:06 -0400

    templating, error catching, energy functions

--- examples/phase_transitions/cahn-hilliard/convex_splitting/cahn-hilliard.cpp
+++ examples/phase_transitions/cahn-hilliard/convex_splitting/cahn-hilliard.cpp
@@ -1,6 +1,6 @@
 // cahn-hilliard.hpp
-// Algorithms for 2D and 3D Cahn-Hilliard model
-// Questions/comments to gruberja@gmail.com (Jason Gruber)
+// Algorithms for 2D Cahn-Hilliard model with semi-implicit convex splitting discretization
+// Questions/comments to trevor.keller@gmail.com (Trevor Keller)
 
 #ifndef CAHNHILLIARD_UPDATE
 #define CAHNHILLIARD_UPDATE
@@ -9,12 +9,30 @@
 #include"cahn-hilliard.hpp"
 #include"../energy.hpp"
 
+const double AA = 0.25*(B + Ca + Cb);
+const double BB = B*Cm + pow(Ca,2) + pow(Cb,2);
+const double CC = 0.5*(A - 3.0*B*pow(Cm,2) - 3.0*pow(Ca,3) - 3.0*pow(Cb,3));
+const double DD = A*Cm - B*pow(Cm,3) - pow(Ca,4) - pow(Cb,4);
+const double EE = 0.5*(A*pow(Cm,2) - 0.5*B*pow(Cm,4) - 0.5*pow(Ca,5) - 0.5*pow(Cb,5));
+
+const double deltaX = 1.0;
+const double CFL = 25.0;
+const double dt = pow(deltaX, 4)*CFL/(32.0*D*K);
+
+double expansive_dfdc(const double& C)
+{
+    return -A*(C-Cm) + B*pow(C-Cm, 3) + Ca*pow(C-Ca, 3) + Cb*pow(C-Cb, 3);
+}
+
 namespace MMSP {
 
 void generate(int dim, const char* filename)
 {
+
+    // Initial conditions after CHiMaD Phase Field Workshop benchmark problem (October 2015)
+
 	if (dim!=2) {
-		std::cerr<<"ERROR: CHiMaD problems are 2-D, only!"<<std::endl;
+		std::cerr<<"ERROR: Convex splitting discretization is only 2-D, for now."<<std::endl;
 		std::exit(-1);
 	}
 
@@ -23,16 +41,31 @@ void generate(int dim, const char* filename)
 	rank = MPI::COMM_WORLD.Get_rank();
 	#endif
 
+    if (AA<0) {
+        if (rank==0)
+            std::cerr<<"Error: parameter AA is negative. Double-check your constants."<<std::endl;
+            std::exit(-1);
+    } else if (BB<0) {
+        if (rank==0)
+            std::cerr<<"Error: parameter BB is negative. Double-check your constants."<<std::endl;
+        std::exit(-1);
+    } else if (CC<0) {
+        if (rank==0)
+            std::cerr<<"Error: parameter CC is negative. Double-check your constants."<<std::endl;
+        std::exit(-1);
+    } // DD and EE don't matter, since their convexity is not in question.
+
     const double q[2] = {0.1*std::sqrt(2.0), 0.1*std::sqrt(3.0)};
 
 	if (dim==2) {
-		MMSP::grid<2,double> grid(1,0,200,0,200);
+		MMSP::grid<2,vector<double> > grid(2,0,200,0,200);
 		for (int d=0; d<dim; d++)
 			dx(grid,d) = deltaX;
 
 		for (int i=0; i<nodes(grid); i++) {
 			MMSP::vector<int> x = position(grid,i);
-			grid(x) = 0.45 + 0.01 * std::cos(x[0]*dx(grid,0)*q[0] + x[1]*dx(grid,1)*q[1]);
+			grid(x)[0] = 0.45 + 0.01 * std::cos(x[0]*dx(grid,0)*q[0] + x[1]*dx(grid,1)*q[1]);
+			grid(x)[1] = 1.0;
 		}
 
 		#ifdef MPI_VERSION
@@ -45,7 +78,7 @@ void generate(int dim, const char* filename)
 }
 
 template <int dim, typename T>
-void update(MMSP::grid<dim,T>& grid, int steps)
+void update(MMSP::grid<dim,vector<T> >& grid, int steps)
 {
 	int rank=0;
 	#ifdef MPI_VERSION
@@ -54,8 +87,8 @@ void update(MMSP::grid<dim,T>& grid, int steps)
 
     ghostswap(grid);
 
-	MMSP::grid<dim,T> update(grid);
-	MMSP::grid<dim,T> temp(grid);
+	MMSP::grid<dim,vector<T> > update(grid);
+	MMSP::grid<dim,vector<T> > temp(grid);
 	// Make sure the grid spacing is correct
 	for (int d=0; d<dim; d++) {
 		dx(grid,d) = deltaX;
@@ -66,8 +99,8 @@ void update(MMSP::grid<dim,T>& grid, int steps)
 	for (int step=0; step<steps; step++) {
 		for (int i=0; i<nodes(grid); i++) {
 			MMSP::vector<int> x = position(grid,i);
-			double c = grid(x);
-			temp(x) = dfdc(c) - K*laplacian(grid,x);
+			double c = grid(x)[0];
+			temp(x)[0] = dfdc(c) - K*laplacian(grid,x)[0];
 		}
 		#ifdef MPI_VERSION
 		MPI::COMM_WORLD.Barrier();
@@ -78,9 +111,9 @@ void update(MMSP::grid<dim,T>& grid, int steps)
 		double mass = 0.0;
 		for (int i=0; i<nodes(grid); i++) {
 			MMSP::vector<int> x = position(grid,i);
-			update(x) = grid(x) + dt*D*laplacian(temp,x);
-			energy += dx(grid)*dy(grid)*energydensity(update(x));
-			mass += dx(grid)*dy(grid)*update(x);
+			update(x)[0] = grid(x)[0] + dt*D*laplacian(temp,x)[0];
+			energy += dx(grid)*dy(grid)*energydensity(update(x)[0]);
+			mass += dx(grid)*dy(grid)*update(x)[0];
 		}
 		#ifdef MPI_VERSION
 		MPI::COMM_WORLD.Barrier();
--- examples/phase_transitions/cahn-hilliard/convex_splitting/cahn-hilliard.hpp
+++ examples/phase_transitions/cahn-hilliard/convex_splitting/cahn-hilliard.hpp
@@ -5,7 +5,7 @@
 std::string PROGRAM = "cahn-hilliard";
 std::string MESSAGE = "Cahn-Hilliard model using MMSP";
 
-typedef MMSP::grid<1,double> GRID1D;
-typedef MMSP::grid<2,double> GRID2D;
-typedef MMSP::grid<3,double> GRID3D;
+typedef MMSP::grid<1,MMSP::vector<double> > GRID1D;
+typedef MMSP::grid<2,MMSP::vector<double> > GRID2D;
+typedef MMSP::grid<3,MMSP::vector<double> > GRID3D;
 
