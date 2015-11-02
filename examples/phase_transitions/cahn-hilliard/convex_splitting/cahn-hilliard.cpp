fc9824088166523564a9c2a6730a32254b657e91
Trevor Keller <trevor.keller@nist.gov>
2015-11-02 11:06:03 -0500

    add comments, tracking down logical flaws

--- examples/phase_transitions/cahn-hilliard/convex_splitting/cahn-hilliard.cpp
+++ examples/phase_transitions/cahn-hilliard/convex_splitting/cahn-hilliard.cpp
@@ -9,9 +9,14 @@
 #include"cahn-hilliard.hpp"
 #include"../energy.hpp"
 
+// Parametrize free energy density
+// F(c) = AA*c^4  - BB*c^3 - CC*c^2 + DD*c - EE
+// Simplest form sets AA=0.25, CC=0.5, BB=DD=EE=0 --> F(c) = 0.25*c^4 - 0.5*c^2,
+//                                                    f(c) = c^3 - c^2.
+
 const double AA = 0.25;// 0.25*(B + Ca + Cb);
 const double BB = 0.0;// B*Cm + pow(Ca,2) + pow(Cb,2);
-const double CC = 0.5;// 0.5*(3.0*B*pow(Cm,2) + 3.0*pow(Ca,3) + 3.0*pow(Cb,3) - A);
+const double CC = 0.5;// 0.5*(A - 3.0*B*pow(Cm,2) - 3.0*pow(Ca,3) - 3.0*pow(Cb,3));
 const double DD = 0.0;// A*Cm - B*pow(Cm,3) - pow(Ca,4) - pow(Cb,4);
 const double EE = 0.0;// 0.5*(A*pow(Cm,2) - 0.5*B*pow(Cm,4) - 0.5*pow(Ca,5) - 0.5*pow(Cb,5));
 
@@ -20,14 +25,22 @@ const double CFL = 25.0;
 const double dt = pow(deltaX, 4)*CFL/(32.0*D*K);
 const double tolerance = 1.0e-8; // "Fair" value. 1e-5 is poor, 1e-12 is publication-quality but slow.
 
-double parametric_energy_density(const double& C)
+template <typename T>
+double parametric_energy_density(const T& C)
 {
-    return AA*pow(C,4) - BB*pow(C,3) - CC*pow(C,2) + DD*C - EE;
+    return (AA*pow(C,4) - BB*pow(C,3) - CC*pow(C,2) + DD*C - EE);
 }
 
-double expansive_dfdc(const double& C)
+template <typename T>
+double parametric_dfdc(const T& C)
 {
-    return DD - 3.0*BB*pow(C,3) - 2.0*CC*pow(C,2);
+    return (4.0*AA*pow(C,3) - 3.0*BB*pow(C,2) - 2.0*CC*C + DD);
+}
+
+template <typename T>
+double expansive_dfdc(const T& C)
+{
+    return (DD - 3.0*BB*pow(C,3) - 2.0*CC*pow(C,2));
 }
 
 namespace MMSP {
@@ -56,7 +69,7 @@ double field_laplacian(const grid<dim,vector<T> >& GRID, const vector<int>& x, c
 
 // Define a Laplacian function missing the central value, for implicit source terms
 template<int dim, typename T>
-double ring_laplacian(const grid<dim,vector<T> >& GRID, const vector<int>& x, const int field)
+double fringe_laplacian(const grid<dim,vector<T> >& GRID, const vector<int>& x, const int field)
 {
   T laplacian(0.0);
   vector<int> s = x;
@@ -92,16 +105,13 @@ void generate(int dim, const char* filename)
     std::cout<<"f(C) = ("<<AA<<")C^4 - ("<<BB<<")C^3 - ("<<CC<<")C^2 + ("<<DD<<")C - ("<<EE<<')'<<std::endl;
     if (AA<0) {
         if (rank==0)
-            std::cerr<<"Error: parameter AA is negative. Double-check your constants."<<std::endl;
-            std::exit(-1);
+            std::cerr<<"Warning: parameter AA is negative. Double-check your constants."<<std::endl;
     } else if (BB<0) {
         if (rank==0)
-            std::cerr<<"Error: parameter BB is negative. Double-check your constants."<<std::endl;
-        std::exit(-1);
+            std::cerr<<"Warning: parameter BB is negative. Double-check your constants."<<std::endl;
     } else if (CC<0) {
         if (rank==0)
-            std::cerr<<"Error: parameter CC is negative. Double-check your constants."<<std::endl;
-        std::exit(-1);
+            std::cerr<<"Warning: parameter CC is negative. Double-check your constants."<<std::endl;
     } // DD and EE don't matter, since their convexity is not in question.
 
     const double q[2] = {0.1*std::sqrt(2.0), 0.1*std::sqrt(3.0)};
@@ -114,7 +124,11 @@ void generate(int dim, const char* filename)
 		for (int i=0; i<nodes(grid); i++) {
 			MMSP::vector<int> x = position(grid,i);
 			grid(x)[0] = 0.45 + 0.01 * std::cos(x[0]*dx(grid,0)*q[0] + x[1]*dx(grid,1)*q[1]);
-			grid(x)[1] = 1.0;
+		}
+		for (int i=0; i<nodes(grid); i++) {
+			MMSP::vector<int> x = position(grid,i);
+			double lap = field_laplacian(grid, x, 0);
+			grid(x)[1] = parametric_dfdc(grid(x)[0]) - K*lap;
 		}
 
 		#ifdef MPI_VERSION
@@ -139,7 +153,7 @@ void update(MMSP::grid<dim,vector<T> >& grid, int steps)
 	MMSP::grid<dim,vector<T> > update(grid); // new values at each point and initial guess for iteration
 	MMSP::grid<dim,vector<T> > guess(grid); // new guess for iteration
 
-	// Make sure the grid spacing is correct and isotropic
+	// Make sure the grid spacing is correct. Modify at will.
 	for (int d=0; d<dim; d++) {
 		dx(grid,d) = deltaX;
 		dx(update,d) = deltaX;
@@ -153,40 +167,63 @@ void update(MMSP::grid<dim,vector<T> >& grid, int steps)
 	MPI::COMM_WORLD.Allreduce(&localGridSize, &gridSize, 1, MPI_DOUBLE, MPI_SUM);
 	#endif
 
-	for (int step=0; step<steps; step++) {
+    double lapWeight = 0.0;
+    for (int d=0; d<dim; d++)
+        lapWeight += 2.0/pow(dx(grid,d),2);
 
-        update.copy(grid); // deep copy: includes data and ghost cells
+    update.copy(grid); // deep copy: includes data and ghost cells
+    /*for (int n=0; n<nodes(update); n++) {
+  		MMSP::vector<int> x = position(grid,n);
+        update(x)[0] = 0.0;
+        update(x)[1] = grid(x)[1];
+    }*/
+
+	for (int step=0; step<steps; step++) {
 
         double residual=1.0;
         unsigned int iter=0;
 
+        // Iteratively solve AX=B until ||B-AX|| / ||B|| < tolerance
         while (residual>tolerance) {
             residual = 0.0;
+
+            // update stores the old gues, Jacobi lexicographic index l
+            // guess  stores the new gues, Jacobi lexicographic index l+1
+
     		for (int n=0; n<nodes(grid); n++) {
 	    		MMSP::vector<int> x = position(grid,n);
 
-	    		const double S1 = grid(x)[0] + dt*D*ring_laplacian(update, x, 1);
-	    		const double S2 = DD - 3.0*BB*pow(grid(x)[0],2) - 2.0*CC*grid(x)[0] - K*ring_laplacian(update, x, 0);
-	    		const double A12 = (2.0*dim)*dt*D/pow(deltaX,2);
-	    		const double A21 = -(2.0*dim)*K/pow(deltaX,2) - 4.0*AA*pow(update(x)[0],2);
+	    		const double B1 = grid(x)[0] + dt*D*fringe_laplacian(update, x, 1);
+	    		const double B2 = expansive_dfdc(grid(x)[0]) - K*fringe_laplacian(update, x, 0);
+	    		const double A11 = 1.0;                                       const double A12 = dt*D*lapWeight;
+	    		const double A21 = -K*lapWeight - 4.0*AA*pow(update(x)[0],2); const double A22 = 1.0;
 
 	    		// Apply Cramer's Rule to the [2x2][2]=[2] matrix equation
-	    		const double cramerDenom = 1.0 - A12*A21; // A11*A22 = 1*1 = 1
-	    		const double cramerX1 = (S1 - S2*A12)/cramerDenom;
-	    		const double cramerX2 = (S2 - A21*S1)/cramerDenom;
+	    		const double denom = A11*A22 - A12*A21; // N.B.: A11*A22 = 1*1 = 1
+	    		const double newX1 =( B1*A22 -  B2*A12)/denom;
+	    		const double newX2 =(A11*B2  - A21*B1 )/denom;
 
                 // Copy values to guess. Forgive the mismatched C (0-indexed) and matrix (1-indexed) notations.
-                guess(x)[0] = cramerX1;
-                guess(x)[1] = cramerX2;
+                guess(x)[0] = newX1; // c
+                guess(x)[1] = newX2; // mu
 
-                // Compute ||b-Ax|| / ||b||
-                const double AX1 = 1.0*cramerX1 + A12*cramerX2;
-                const double AX2 = A21*cramerX1 + 1.0*cramerX2;
-                const double normBminusAx = std::sqrt(pow(S1-AX1,2) + pow(S2-AX2,2));
-                //const double normB = std::sqrt(pow(S1,2)+pow(S2,2));
-                const double normB = std::sqrt(pow(grid(x)[0],2)+pow(DD - 3.0*BB*pow(grid(x)[0],2) - 2.0*CC*grid(x)[0],2));
+                // Compute ||B-AX|| / ||B|| using L2 vector norms
+                const double AX1 = A11*newX1 + A12*newX2;
+                const double AX2 = A21*newX1 + A22*newX2;
+                const double normBminusAX = std::sqrt(pow(B1-AX1,2) + pow(B2-AX2,2));
+                const double normB = std::sqrt(pow(B1,2)+pow(B2,2));
+                //const double normB = std::sqrt(pow(grid(x)[0],2) + pow(expansive_dfdc(grid(x)[0]),2));
 
-                residual += normBminusAx/(normB*gridSize);
+                residual += normBminusAX/(normB*gridSize);
+                //residual += normBminusAx/gridSize;
+
+                /*
+                #ifdef DEBUG
+                if (n==nodes(grid)/2)
+                    if (rank==0)
+                        std::printf("        |  1        %2.6f | = |  %2.6f |\n        | %2.6f 1        | = | %2.6f |\n",A12,B1,A21,B2);
+                #endif
+                */
 	    	}
 
             swap(update, guess);
@@ -206,7 +243,7 @@ void update(MMSP::grid<dim,vector<T> >& grid, int steps)
    		    #endif
         }
 
-		swap(grid,update);
+		swap(grid,guess);
 		ghostswap(grid);
 
   		#ifndef DEBUG
