477648a0cdb2074489799f48f2fc8ab913586090
Trevor Keller <trevor.keller@nist.gov>
2015-11-02 12:20:01 -0500

    Residual should check whether a new iteration is warranted: compute using new value and last guess

--- examples/phase_transitions/cahn-hilliard/convex_splitting/cahn-hilliard.cpp
+++ examples/phase_transitions/cahn-hilliard/convex_splitting/cahn-hilliard.cpp
@@ -21,24 +21,24 @@ const double DD = 0.0;// A*Cm - B*pow(Cm,3) - pow(Ca,4) - pow(Cb,4);
 const double EE = 0.0;// 0.5*(A*pow(Cm,2) - 0.5*B*pow(Cm,4) - 0.5*pow(Ca,5) - 0.5*pow(Cb,5));
 
 const double deltaX = 1.0;
-const double CFL = 25.0;
+const double CFL = 20.0;
 const double dt = pow(deltaX, 4)*CFL/(32.0*D*K);
 const double tolerance = 1.0e-8; // "Fair" value. 1e-5 is poor, 1e-12 is publication-quality but slow.
 
 template <typename T>
-double parametric_energy_density(const T& C)
+T parametric_energy_density(const T& C)
 {
     return (AA*pow(C,4) - BB*pow(C,3) - CC*pow(C,2) + DD*C - EE);
 }
 
 template <typename T>
-double parametric_dfdc(const T& C)
+T parametric_dfdc(const T& C)
 {
     return (4.0*AA*pow(C,3) - 3.0*BB*pow(C,2) - 2.0*CC*C + DD);
 }
 
 template <typename T>
-double expansive_dfdc(const T& C)
+T expansive_dfdc(const T& C)
 {
     return (DD - 3.0*BB*pow(C,3) - 2.0*CC*pow(C,2));
 }
@@ -47,7 +47,7 @@ namespace MMSP {
 
 // Define a Laplacian function for a specific field
 template<int dim, typename T>
-double field_laplacian(const grid<dim,vector<T> >& GRID, const vector<int>& x, const int field)
+T field_laplacian(const grid<dim,vector<T> >& GRID, const vector<int>& x, const int field)
 {
   T laplacian(0.0);
   vector<int> s = x;
@@ -69,7 +69,7 @@ double field_laplacian(const grid<dim,vector<T> >& GRID, const vector<int>& x, c
 
 // Define a Laplacian function missing the central value, for implicit source terms
 template<int dim, typename T>
-double fringe_laplacian(const grid<dim,vector<T> >& GRID, const vector<int>& x, const int field)
+T fringe_laplacian(const grid<dim,vector<T> >& GRID, const vector<int>& x, const int field)
 {
   T laplacian(0.0);
   vector<int> s = x;
@@ -102,16 +102,16 @@ void generate(int dim, const char* filename)
 	rank = MPI::COMM_WORLD.Get_rank();
 	#endif
 
-    std::cout<<"f(C) = ("<<AA<<")C^4 - ("<<BB<<")C^3 - ("<<CC<<")C^2 + ("<<DD<<")C - ("<<EE<<')'<<std::endl;
+    std::cout<<"F(C) = ("<<AA<<")C^4 - ("<<BB<<")C^3 - ("<<CC<<")C^2 + ("<<DD<<")C - ("<<EE<<')'<<std::endl;
     if (AA<0) {
         if (rank==0)
-            std::cerr<<"Warning: parameter AA is negative. Double-check your constants."<<std::endl;
+            std::cerr<<"Warning: parameter AA is negative. Double-check your inputs."<<std::endl;
     } else if (BB<0) {
         if (rank==0)
-            std::cerr<<"Warning: parameter BB is negative. Double-check your constants."<<std::endl;
+            std::cerr<<"Warning: parameter BB is negative. Double-check your inputs."<<std::endl;
     } else if (CC<0) {
         if (rank==0)
-            std::cerr<<"Warning: parameter CC is negative. Double-check your constants."<<std::endl;
+            std::cerr<<"Warning: parameter CC is negative. Double-check your inputs."<<std::endl;
     } // DD and EE don't matter, since their convexity is not in question.
 
     const double q[2] = {0.1*std::sqrt(2.0), 0.1*std::sqrt(3.0)};
@@ -183,13 +183,11 @@ void update(MMSP::grid<dim,vector<T> >& grid, int steps)
         double residual=1.0;
         unsigned int iter=0;
 
-        // Iteratively solve AX=B until ||B-AX|| / ||B|| < tolerance
         while (residual>tolerance) {
-            residual = 0.0;
-
             // update stores the old gues, Jacobi lexicographic index l
             // guess  stores the new gues, Jacobi lexicographic index l+1
 
+            // Solve AX=B using Cramer's rule
     		for (int n=0; n<nodes(grid); n++) {
 	    		MMSP::vector<int> x = position(grid,n);
 
@@ -198,24 +196,29 @@ void update(MMSP::grid<dim,vector<T> >& grid, int steps)
 	    		const double A11 = 1.0;                                       const double A12 = dt*D*lapWeight;
 	    		const double A21 = -K*lapWeight - 4.0*AA*pow(update(x)[0],2); const double A22 = 1.0;
 
-	    		// Apply Cramer's Rule to the [2x2][2]=[2] matrix equation
-	    		const double denom = A11*A22 - A12*A21; // N.B.: A11*A22 = 1*1 = 1
-	    		const double newX1 =( B1*A22 -  B2*A12)/denom;
-	    		const double newX2 =(A11*B2  - A21*B1 )/denom;
+	    		const double denom = A11*A22 - A12*A21;
+	    		guess(x)[0] =( B1*A22 -  B2*A12)/denom;
+	    		guess(x)[1] =(A11*B2  - A21*B1 )/denom;
+             }
 
-                // Copy values to guess. Forgive the mismatched C (0-indexed) and matrix (1-indexed) notations.
-                guess(x)[0] = newX1; // c
-                guess(x)[1] = newX2; // mu
+            // Compute ||B-AX|| / ||B|| using L2 vector norms
+            residual = 0.0;
+    		for (int n=0; n<nodes(grid); n++) {
+	    		MMSP::vector<int> x = position(grid,n);
 
-                // Compute ||B-AX|| / ||B|| using L2 vector norms
-                const double AX1 = A11*newX1 + A12*newX2;
-                const double AX2 = A21*newX1 + A22*newX2;
+	    		const double B1 = grid(x)[0] + dt*D*fringe_laplacian(guess, x, 1);
+	    		const double B2 = expansive_dfdc(grid(x)[0]) - K*fringe_laplacian(guess, x, 0);
+	    		const double A11 = 1.0;                                      const double A12 = dt*D*lapWeight;
+	    		const double A21 = -K*lapWeight - 4.0*AA*pow(guess(x)[0],2); const double A22 = 1.0;
+
+                const double AX1 = A11*guess(x)[0] + A12*guess(x)[1];
+                const double AX2 = A21*guess(x)[0] + A22*guess(x)[1];
                 const double normBminusAX = std::sqrt(pow(B1-AX1,2) + pow(B2-AX2,2));
-                const double normB = std::sqrt(pow(B1,2)+pow(B2,2));
-                //const double normB = std::sqrt(pow(grid(x)[0],2) + pow(expansive_dfdc(grid(x)[0]),2));
+                //const double normB = std::sqrt(pow(B1,2)+pow(B2,2));
+                const double normB = std::sqrt(pow(update(x)[0],2) + pow(expansive_dfdc(update(x)[0]),2));
 
                 residual += normBminusAX/(normB*gridSize);
-                //residual += normBminusAx/gridSize;
+                //residual += normBminusAX/gridSize;
 
                 /*
                 #ifdef DEBUG
