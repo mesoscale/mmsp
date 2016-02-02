d361289817af2b0dc15d8d3ffa637bdbed9779b1
Trevor Keller <trevor.keller@nist.gov>
2016-02-02 18:27:48 -0500

    build examples as a simple sanity check
    More elegant and continuous-integration-compatible approaches exist.
    This is a quick & dirty spot check of whether the examples compile.

---
+++ test/build_examples.sh
@@ -0,0 +1,226 @@
+#!/bin/bash
+
+cd ../examples
+
+while [[ $# > 0 ]]
+do
+	key="$1"
+	case $key in
+		-c|--clean)
+		CLEAN=true
+		shift # past argument
+		;;
+		*)
+		# unknown option
+		echo "WARNING: Unknown option ${key}."
+		echo
+    	;;
+	esac
+	shift # past argument or value
+done
+
+if [[ $CLEAN ]]; then
+	echo "Building examples in serial and parallel, then deleting binaries."
+else
+	echo "Building examples in serial and parallel, leaving binaries behind (specify --clean to clean up)."
+fi
+echo
+
+examples=$(pwd)
+
+cd beginners_diffusion/
+pwd
+	make -Bs || exit $?
+	if $CLEAN; then make -s clean; fi
+	cd $examples
+
+echo
+cd coarsening
+pwd
+cd grain_growth
+pwd
+cd anisotropic
+pwd
+	cd Monte_Carlo
+	pwd
+	make -Bs || exit $?
+	make -Bs parallel || exit $?
+	if $CLEAN; then make -s clean; fi
+	cd ../phase_field/
+	pwd
+	make -Bs || exit $?
+	make -Bs parallel || exit $?
+	if $CLEAN; then make -s clean; fi
+	cd ../sparsePF/
+	pwd
+	make -Bs || exit $?
+	make -Bs parallel || exit $?
+	if $CLEAN; then make -s clean; fi
+	cd $examples
+
+cd coarsening/grain_growth/isotropic
+pwd
+	cd Monte_Carlo/
+	pwd
+	make -Bs || exit $?
+	make -Bs parallel || exit $?
+	if $CLEAN; then make -s clean; fi
+	cd ../phase_field/
+	pwd
+	make -Bs || exit $?
+	make -Bs parallel || exit $?
+	if $CLEAN; then make -s clean; fi
+	cd ../sparsePF/
+	pwd
+	make -Bs || exit $?
+	make -Bs parallel || exit $?
+	if $CLEAN; then make -s clean; fi
+	cd $examples
+
+echo
+cd coarsening/ostwald_ripening
+pwd
+cd isotropic
+pwd
+	cd phase_field/
+	pwd
+	make -Bs || exit $?
+	make -Bs parallel || exit $?
+	if $CLEAN; then make -s clean; fi
+	cd $examples
+
+echo
+cd coarsening/zener_pinning
+pwd
+cd anisotropic
+pwd
+	cd Monte_Carlo/
+	pwd
+	make -Bs || exit $?
+	make -Bs parallel || exit $?
+	if $CLEAN; then make -s clean; fi
+	cd ../phase_field/
+	pwd
+	make -Bs || exit $?
+	make -Bs parallel || exit $?
+	if $CLEAN; then make -s clean; fi
+	cd ../sparsePF/
+	pwd
+	make -Bs || exit $?
+	make -Bs parallel || exit $?
+	if $CLEAN; then make -s clean; fi
+	cd $examples
+
+cd coarsening/zener_pinning/isotropic
+pwd
+	cd Monte_Carlo/
+	pwd
+	make -Bs || exit $?
+	make -Bs parallel || exit $?
+	if $CLEAN; then make -s clean; fi
+	cd ../phase_field/
+	pwd
+	make -Bs || exit $?
+	make -Bs parallel || exit $?
+	if $CLEAN; then make -s clean; fi
+	cd ../sparsePF/
+	pwd
+	make -Bs || exit $?
+	make -Bs parallel || exit $?
+	if $CLEAN; then make -s clean; fi
+	cd $examples
+
+echo
+cd differential_equations
+pwd
+cd elliptic
+pwd
+	cd Poisson/
+	pwd
+	make -Bs || exit $?
+	make -Bs parallel || exit $?
+	if $CLEAN; then make -s clean; fi
+	cd $examples
+
+echo
+cd phase_transitions
+pwd
+	cd allen-cahn/
+	pwd
+	make -Bs || exit $?
+	make -Bs parallel || exit $?
+	if $CLEAN; then make -s clean; fi
+	cd $examples
+
+	cd phase_transitions/cahn-hilliard
+	pwd
+	cd convex_splitting/
+	pwd
+	make -Bs || exit $?
+	make -Bs parallel || exit $?
+	if $CLEAN; then make -s clean; fi
+	cd ../explicit/
+	pwd
+	make -Bs || exit $?
+	make -Bs parallel || exit $?
+	if $CLEAN; then make -s clean; fi
+	cd $examples
+
+	cd phase_transitions/model_A
+	pwd
+	make -Bs || exit $?
+	make -Bs parallel || exit $?
+	if $CLEAN; then make -s clean; fi
+	cd $examples
+
+	cd phase_transitions/model_B/
+	pwd
+	make -Bs || exit $?
+	make -Bs parallel || exit $?
+	if $CLEAN; then make -s clean; fi
+	cd $examples
+
+	cd phase_transitions/spinodal/
+	pwd
+	make -Bs || exit $?
+	make -Bs parallel || exit $?
+	if $CLEAN; then make -s clean; fi
+	cd $examples
+
+cd phase_transitions/solidification
+pwd
+	cd anisotropic/
+	pwd
+	make -Bs || exit $?
+	make -Bs parallel || exit $?
+	if $CLEAN; then make -s clean; fi
+	cd $examples
+
+echo
+cd statistical_mechanics
+pwd
+	cd Heisenberg/
+	pwd
+	make -Bs || exit $?
+	make -Bs parallel || exit $?
+	if $CLEAN; then make -s clean; fi
+	cd $examples
+
+	cd statistical_mechanics/Ising/
+	pwd
+	make -Bs || exit $?
+	make -Bs parallel || exit $?
+	if $CLEAN; then make -s clean; fi
+	cd $examples
+
+	cd statistical_mechanics/Potts/
+	pwd
+	make -Bs || exit $?
+	make -Bs parallel || exit $?
+	if $CLEAN; then make -s clean; fi
+	cd $examples
+
+echo
+echo "Examples built successfully."
+
+cd ../test/
