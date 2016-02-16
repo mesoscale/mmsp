e1d90c92d3cd9d53f21d902bb48ee1c993fd8bd7
Trevor Keller <trevor.keller@nist.gov>
2016-02-16 12:29:32 -0500

    constructive comments in test script

--- test/build_examples.sh
+++ test/build_examples.sh
@@ -1,14 +1,46 @@
 #!/bin/bash
 
+# build_examples.sh
+# This test script compiles each canonical* example in serial and parallel
+# then executes the example program in 2D. The length of the test can be
+# controlled by selecting default (1,000 steps), long (5,000 steps), or extra
+# long (25,000 steps). The resulting checkpoints are then converted to PNG
+# images to spot check problems in the code or environment, including execution
+# errors and numerical instabilities. If you see something, say something.
+#
+# Questions/comments to trevor.keller@gmail.com (Trevor Keller)
+
+# * Canonical examples exclude the following directories:
+# differential_equations/elliptic/Poisson  --  segmentation faults
+# beginners_diffusion  --  does not match execution pattern
+
+# Valid flags are:
+# --noexec: build in serial and parallel, but do not execute
+# --clean:  delete binary files after test completes
+# --purge:  delete binaries, data, and images after test completes
+# --long:   execute tests  5x longer than default
+# --extra:  execute tests 25x longer than default
+
+if [[ $(which mmsp2png) == "" ]]
+then
+	# Necessary check for a valid installation.
+	# Consult doc/MMSP.manual.pdf if this fails.
+	echo "mmsp2png utility not found. Please check your installation."
+fi
+
+# Initialize timer and completion counters
 tstart=$(date +%s)
 nSerBld=0
 nParBld=0
 nParRun=0
+
+# Set execution parameters
+EXEC=true
 ITERS=1000
 INTER=500
 CORES=4
-EXEC=true
 
+# Get going
 cd ../examples
 examples=$(pwd)
 
@@ -43,12 +75,11 @@ do
 		INTER=5000
 		;;
 		*)
-		# unknown option
 		echo "WARNING: Unknown option ${key}."
 		echo
     	;;
 	esac
-	shift # past argument or value
+	shift # pop first entry from command-line argument list, reduce $# by 1
 done
 
 echo
@@ -67,6 +98,7 @@ exdirs=("coarsening/grain_growth/anisotropic/Monte_Carlo/" \
 "coarsening/zener_pinning/isotropic/phase_field/" \
 "coarsening/zener_pinning/isotropic/sparsePF/" \
 "phase_transitions/allen-cahn/" \
+"phase_transitions/cahn-hilliard/convex_splitting/" \
 "phase_transitions/cahn-hilliard/explicit/" \
 "phase_transitions/model_A/" \
 "phase_transitions/model_B/" \
@@ -76,10 +108,6 @@ exdirs=("coarsening/grain_growth/anisotropic/Monte_Carlo/" \
 "statistical_mechanics/Ising/" \
 "statistical_mechanics/Potts/")
 
-# Skip phase_transitions/cahn-hilliard/convex_splitting  --  too long
-# Skip differential_equations/elliptic/Poisson  --  segmentation faults
-# Skip beginners_diffusion  --  does not match execution pattern
-
 n=${#exdirs[@]}
 for (( i=0; i<$n; i++ ))
 do
@@ -87,18 +115,25 @@ do
 	j=$(($i+1))
 	cd $examples/${exdirs[$i]}
 	printf "%2d/%2d %-50s\t" $j $n ${exdirs[$i]}
-
-	(make -Bs || exit $?) && ((nSerBld++))
-	(make -Bs parallel || exit $?) && ((nParBld++))
+	echo $(date) >test.log
+	(make -B >>test.log || exit $?) && ((nSerBld++))
+	(make -B parallel >>test.log || exit $?) && ((nParBld++))
 	if [[ $EXEC ]]
 	then
-		rm test.*.png
-		mpirun -np $CORES ./parallel --example 2 test.0000.dat && mpirun -np $CORES ./parallel test.0000.dat $ITERS $INTER >/dev/null && ((nParRun++))
+		# Remove existing images first. If the test fails,
+		# no images in the directory is an obvious red flag.
+		rm -f test.*.png
+		# Run the example in parallel, for speed.
+		mpirun -np $CORES ./parallel --example 2 test.0000.dat >>test.log \
+		&& mpirun -np $CORES ./parallel test.0000.dat $ITERS $INTER >>test.log \
+		&& ((nParRun++))
+		# Show the result
 		for f in *.dat
 		do
-			mmsp2png --zoom $f >/dev/null
+			mmsp2png --zoom $f >>test.log
 		done
 	fi
+	# Clean up binaries and images
 	if [[ $CLEAN ]]
 	then
 		make -s clean
@@ -106,6 +141,7 @@ do
 		if [[ $PURGE ]]
 		then
 			rm test.*.png
+			rm test.log
 		fi
 	fi
 
