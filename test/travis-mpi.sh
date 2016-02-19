7795e359b6e8879bb16bec269403b5b8596e7516
Trevor Keller <trevor.keller@gmail.com>
2016-02-19 00:36:38 -0500

    more pkg mgmt

--- test/travis-mpi.sh
+++ test/travis-mpi.sh
@@ -5,9 +5,9 @@
 set -e
 case $1 in
   mpich2) set -x;
-    sudo apt-get install -q mpich2 libmpich2-dev;;
+    sudo apt-get -y install -qq mpich2 libmpich2-dev;;
   openmpi) set -x;
-    sudo apt-get install openmpi-bin openmpi-dev;;
+    sudo apt-get -y install -qq openmpi-bin openmpi-dev;;
   *)
     echo "Unknown MPI implementation:" $1; exit 1;;
 esac
