1b1454bf84746b378c7d256bb431087b85c30836
Trevor Keller <trevor.keller@gmail.com>
2016-02-19 00:08:22 -0500

    package mgmt

--- test/travis-mpi.sh
+++ test/travis-mpi.sh
@@ -5,7 +5,7 @@
 set -e
 case $1 in
   mpich2) set -x;
-    sudo apt-get install -q mpich2 libmpich2-3 libmpich2-dev;;
+    sudo apt-get install -q mpich2 libmpich2-dev;;
   openmpi) set -x;
     sudo apt-get install openmpi-bin openmpi-dev;;
   *)
