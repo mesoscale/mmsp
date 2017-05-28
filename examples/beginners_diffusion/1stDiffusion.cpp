118e5c78a7683f3d4ad6eef71f29b0238d7faed1
Dan Lewis <lewisd2@rpi.edu>
2017-05-28 01:06:11 -0400

    First examples done.

--- examples/beginners_diffusion/1stDiffusion.cpp
+++ examples/beginners_diffusion/1stDiffusion.cpp
@@ -6,21 +6,21 @@ int main(int argc, char* argv[])
 {
   Init(argc, argv);
 
-  int nodes;
+  int nx;
   int iterations;
   float dt, diffusionCoefficient, dx;
 
-  nodes = 10;
+  nx = 10;
   iterations = 100;
   diffusionCoefficient = 1.0;
   dx = 1.0;
   dt = dx*dx/diffusionCoefficient/4;
 	  
-  grid<1,scalar<double> > GRID(1,0,nodes);
-  grid<1,scalar<double> > GRID2(1,0,nodes);
+  grid<1,scalar<double> > GRID(1,0,nx);
+  grid<1,scalar<double> > GRID2(1,0,nx);
 
   for (int x=x0(GRID); x<x1(GRID); x++)
-    if (x<nodes/2) {
+    if (x<nx/2) {
       GRID[x]=1;
       GRID2[x]=1;
     } else {
@@ -30,7 +30,7 @@ int main(int argc, char* argv[])
 
   for (int i=0; i<iterations; i++) {
     for (int x=x0(GRID); x<x1(GRID); x++) {
-      if (x==0 || x==nodes-1) {
+      if (x==0 || x==nx-1) {
       }
       else {
 	GRID2[x]=(diffusionCoefficient*dt/dx/dx)*(GRID[x-1]-2*GRID[x]+GRID[x+1])+GRID[x];
--- examples/beginners_diffusion/MMSPDiffusion.cpp
+++ examples/beginners_diffusion/MMSPDiffusion.cpp
@@ -1,30 +1,25 @@
 #include"MMSP.hpp"
 using namespace MMSP;
 
-// Add in Laplacian function that is built in to MMSP
-// Try using a grid of vectors >> one iterated with this code, one with laplacian
-
-
-//we start the program off the same way as before, but this time we do not need the offset length variable
 int main(int argc, char* argv[])
 {
 	Init(argc, argv);
-	int length;
-	int iterate;
 
-	std::cout<<"input couple length"<<std::endl;
-	std::cin>>length;
-	std::cout<<""<<std::endl;
+	int nx;
+	int iterations;
+	float dt, diffusionCoefficient, dx;
 
-	std::cout<<"input number of iterations"<<std::endl;
-	std::cin>>iterate;
-	std::cout<<""<<std::endl;
+	nx = 10;
+	iterations = 100;
+	diffusionCoefficient = 1.0;
+	dx = 1.0;
+	dt = dx*dx/diffusionCoefficient/4;
 
-	grid<1,scalar<double> > GRID(1,0,length);
-	grid<1,scalar<double> > update(1,0,length);
+	grid<1,scalar<double> > GRID(1,0,nx);
+	grid<1,scalar<double> > update(1,0,nx);
 
 	for (int x=x0(GRID); x<x1(GRID); x++)
-		if (x<length/2) {
+		if (x<nx/2) {
 			GRID[x]=1;
 			update[x]=1;
 		} else {
@@ -32,8 +27,6 @@ int main(int argc, char* argv[])
 			update[x]=1;
 		}
 
-	//now we set the boundary conditions of both grids.  By choosing the Dirichlet conditions, it is nearly identical to the manually set boundaires.
-	//the difference is that the first and last nodes of the grid can change, and the theoretical points outside the grid are fixed.
 	b0(GRID,0) = Dirichlet;
 	b1(GRID,0) = Dirichlet;
 	b0(update,0) = Dirichlet;
@@ -41,10 +34,11 @@ int main(int argc, char* argv[])
 
 	ghostswap(GRID);
 
-	for (int k=0; k<iterate; k++) {
+	for (int k=0; k<iterations; k++) {
 		for (int i=0; i<nodes(GRID); i++) {
-			//we can use MMSP's definition for laplacian instead of hard coding it.
-			update(i)=0.5*laplacian(GRID,i)+GRID[i];
+			update(i)=(diffusionCoefficient*dt/dx/dx)*laplacian(GRID,i)+GRID[i];
+			update[x0(GRID)] = 1.0;
+			update[x1(GRID)] = 0.0;
 		}
 		swap(GRID,update);
 		ghostswap(GRID);
